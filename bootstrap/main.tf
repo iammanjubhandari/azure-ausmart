f1054233cace61bc1ce7d8e3c4c665ec9110228a
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-05 21:21:44 +0800

    feat: storage account with blob versioning for state

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -15,3 +15,19 @@ resource "azurerm_resource_group" "tfstate" {
   name     = "ausmart-tfstate-rg"
   location = var.location
 }
+
+resource "azurerm_storage_account" "tfstate" {
+  name                     = "ausmarttfstatedev"
+  resource_group_name      = azurerm_resource_group.tfstate.name
+  location                 = azurerm_resource_group.tfstate.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  blob_properties {
+    versioning_enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
