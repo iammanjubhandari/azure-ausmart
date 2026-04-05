602a5d75007ed3026899980a198e0779c90ba032
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-05 21:34:55 +0800

    add azurerm backend config for remote state ref: #5

--- envs/au-dev/c1-versions.tf
+++ envs/au-dev/c1-versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
 
+  backend "azurerm" {
+    resource_group_name  = "ausmart-tfstate-rg"
+    storage_account_name = "ausmarttfstatedev"
+    container_name       = "tfstate"
+    key                  = "au-dev.terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
