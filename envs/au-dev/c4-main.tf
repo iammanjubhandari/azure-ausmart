9cc5baf00634c200a61e746a0710e5cbfbc7e933
Manjunath Bhandari <iammanjunath.bhandari20@gmail.com>
2026-04-06 14:47:54 +0800

    wire acr and monitoring into au-dev ref: #9

--- envs/au-dev/c4-main.tf
+++ envs/au-dev/c4-main.tf
@@ -15,3 +15,27 @@ module "vnet" {
   vnet_cidr           = var.vnet_cidr
   cluster_name        = var.cluster_name
 }
+
+# --- ACR (AKS needs this for image pulls) ---
+
+module "acr" {
+  source = "../../modules/acr"
+
+  name_prefix         = local.name_prefix
+  common_tags         = local.common_tags
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+}
+
+# --- Monitoring (AKS needs Log Analytics workspace ID) ---
+
+module "monitoring" {
+  source = "../../modules/monitoring"
+
+  name_prefix         = local.name_prefix
+  common_tags         = local.common_tags
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.location
+  alert_email         = var.alert_email
+  monthly_budget      = var.monthly_budget
+}
--- envs/au-dev/c5-outputs.tf
+++ envs/au-dev/c5-outputs.tf
@@ -7,3 +7,8 @@ output "vnet_id" {
   description = "ID of the virtual network"
   value       = module.vnet.vnet_id
 }
+
+output "acr_login_server" {
+  description = "Login server URL for the ACR"
+  value       = module.acr.acr_login_server
+}
